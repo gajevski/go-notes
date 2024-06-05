20d46583f4570e4e0e37a3a69e5b96f65ad45f84
gajevski <mikolajxgajewski@gmail.com>
2024-06-05 14:32:00 +0200

    Initial bubble tea POC

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
 )
 
 type Note struct {
@@ -10,21 +12,98 @@ type Note struct {
 	ID      int    `json:"id"`
 }
 
+type Model struct {
+	choices  []string
+	cursor   int
+	selected map[int]struct{}
+}
+
 func main() {
-	file, err := os.Create("test.txt")
-
-	if err != nil {
-		fmt.Println("Error %w", err)
-		return
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("There's been an error: %v", err)
+		os.Exit(1)
 	}
-	bytes, err := file.WriteString("Test string 2")
+}
 
-	if err != nil {
-		fmt.Println("Error %w", err)
-		file.Close()
-		return
+func initialModel() Model {
+	return Model{
+		choices:  []string{"Buy carrots", "Buy celery", "Buy kohlrabi"},
+		selected: make(map[int]struct{}),
+	}
+}
+
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	case tea.KeyMsg:
+
+		// Cool, what was the actual key pressed?
+		switch msg.String() {
+
+		// These keys should exit the program.
+		case "ctrl+c", "q":
+			return m, tea.Quit
+
+		// The "up" and "k" keys move the cursor up
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+
+		// The "down" and "j" keys move the cursor down
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+
+		// The "enter" key and the spacebar (a literal space) toggle
+		// the selected state for the item that the cursor is pointing at.
+		case "enter", " ":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+		}
 	}
 
-	fmt.Println(bytes, "File saved successfully")
+	// Return the updated Model to the Bubble Tea runtime for processing.
+	// Note that we're not returning a command.
+	return m, nil
+}
 
+func (m Model) View() string {
+	// The header
+	s := "What should we buy at the market?\n\n"
+
+	// Iterate over our choices
+	for i, choice := range m.choices {
+
+		// Is the cursor pointing at this choice?
+		cursor := " " // no cursor
+		if m.cursor == i {
+			cursor = ">" // cursor!
+		}
+
+		// Is this choice selected?
+		checked := " " // not selected
+		if _, ok := m.selected[i]; ok {
+			checked = "x" // selected!
+		}
+
+		// Render the row
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	}
+
+	// The footer
+	s += "\nPress q to quit.\n"
+
+	// Send the UI for rendering
+	return s
 }
